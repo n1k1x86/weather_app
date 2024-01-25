ea0eaf1ff0b60b8241b8a17f30108aae626d63dd
n1k1x86 <i@n1k1x86.ru>
2024-01-26 04:56:42 +1000

    [WEATHER-9] extended models for images

--- weather_api/main_api/models.py
+++ weather_api/main_api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Country(models.Model):
     name = models.CharField(default=None, max_length=255)
+    country_image = models.ImageField(upload_to='uploads/country/')
 
     class Meta:
         db_table = 'country'
@@ -11,6 +12,7 @@ class Country(models.Model):
 class City(models.Model):
     city = models.CharField(default=None, max_length=255)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    city_image = models.ImageField(upload_to='uploads/cities/')
     latitude = models.FloatField(default=0.0)
     longitude = models.FloatField(default=0.0)
 
